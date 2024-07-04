5766a00e289b153b45b55e3f1e7428fefc43cfb9
Malvin Opondo <malvin.opondo@student.moringaschool.com>
2024-07-05 00:12:31 +0300

    feat(index.js): renamed our index.js file

--- prisma/index.js
+++ prisma/index.js
@@ -55,3 +55,6 @@ app.delete("/users/:id", async (req, res) => {
   });
   res.json({ message: "User deleted" });
 });
+app.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000');
+  });
