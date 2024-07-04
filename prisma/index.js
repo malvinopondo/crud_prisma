cce126954a7b6b26830c3cf762c9af865919c103
Malvin Opondo <malvin.opondo@student.moringaschool.com>
2024-07-05 00:04:30 +0300

    feat(index.js): delete user by I.D

--- prisma/index.js
+++ prisma/index.js
@@ -1,48 +1,57 @@
-const express = require ('express');
-const bodyParser = require ('body-parser');
-const {PrismaClient} = require('@prisma/client');
+const express = require("express");
+const bodyParser = require("body-parser");
+const { PrismaClient } = require("@prisma/client");
 
 const app = express();
-const prisma = new PrismaClient ();
+const prisma = new PrismaClient();
 
 app.use(bodyParser.json());
 
-app.post ('/users',async (req, res)=>{
-    const {name, email} = req.body;
-    const user = await prisma.user.create({
-        data: {
-            name,
-            email
-        }
-    });
-    res.json(user);
+app.post("/users", async (req, res) => {
+  const { name, email } = req.body;
+  const user = await prisma.user.create({
+    data: {
+      name,
+      email,
+    },
+  });
+  res.json(user);
 });
 
-app.get('/users', async (req, res) => {
-    const users = await prisma.user.findMany();
-    res.json(users);
-  });
+app.get("/users", async (req, res) => {
+  const users = await prisma.user.findMany();
+  res.json(users);
+});
 
-  app.get('/users/:id', async (req, res) => {
-    const { id } = req.params;
-    const user = await prisma.user.findUnique({
-      where: {
-        id: parseInt(id),
-      },
-    });
-    res.json(user);
+app.get("/users/:id", async (req, res) => {
+  const { id } = req.params;
+  const user = await prisma.user.findUnique({
+    where: {
+      id: parseInt(id),
+    },
   });
-  app.put('/users/:id', async (req, res) => {
-    const { id } = req.params;
-    const { name, email } = req.body;
-    const user = await prisma.user.update({
-      where: {
-        id: parseInt(id),
-      },
-      data: {
-        name,
-        email
-      },
-    });
-    res.json(user);
+  res.json(user);
+});
+app.put("/users/:id", async (req, res) => {
+  const { id } = req.params;
+  const { name, email } = req.body;
+  const user = await prisma.user.update({
+    where: {
+      id: parseInt(id),
+    },
+    data: {
+      name,
+      email,
+    },
   });
+  res.json(user);
+});
+app.delete("/users/:id", async (req, res) => {
+  const { id } = req.params;
+  await prisma.user.delete({
+    where: {
+      id: parseInt(id),
+    },
+  });
+  res.json({ message: "User deleted" });
+});
